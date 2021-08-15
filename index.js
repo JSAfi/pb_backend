a7afceb3d67b8a83c8644d43362a6aad7b3cc495
JSAfi <jaakko.m.salmela@gmail.com>
2021-08-15 14:14:09 +0300

    lisätty unique validator

--- index.js
+++ index.js
@@ -34,8 +34,12 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     
     if(error.name === 'CastError') {
-        console.log('joo, castError tuli')
+        console.log('castError!')
         return response.status(400).send({error: 'malformatted id'})
+    } 
+    if(error.name === 'ValidationError') {
+        console.log('validointivirhe!')
+        return response.status(400).send({error: 'name already in phonebook'})
     }
 
     next(error)
